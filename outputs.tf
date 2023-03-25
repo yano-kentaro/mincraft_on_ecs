a1dbd423de0ffec8b4e517e2ef2c50d2b9ea8cc0
ken <ynkn0829@gmail.com>
2023-03-26 08:19:20 +0900

    微修正

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "minecraft_alb_dns_name" {
+output "minecraft_nlb_dns_name" {
   description = "The DNS name of the Application Load Balancer for the Minecraft server"
   value       = aws_lb.minecraft_nlb.dns_name
 }
