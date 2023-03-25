045c265225ce395cda6ec5f31eecf95446c39068
ken <ynkn0829@gmail.com>
2023-03-26 08:45:36 +0900

    ECS Serviceのファイル切り出し

--- aws_ecs.tf
+++ aws_ecs.tf
@@ -11,6 +11,41 @@ resource "aws_ecs_cluster" "minecraft_cluster" {
 }
 
 # ---------------------------------------------
+# ECS　Service
+# ---------------------------------------------
+resource "aws_ecs_service" "minecraft_service" {
+  name            = "minecraft_service"
+  cluster         = aws_ecs_cluster.minecraft_cluster.id
+  task_definition = aws_ecs_task_definition.minecraft_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets = [
+      aws_subnet.minecraft_subnet_1.id,
+      aws_subnet.minecraft_subnet_2.id
+    ]
+    security_groups  = [aws_security_group.minecraft_sg.id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.minecraft_target_group.arn
+    container_name   = "minecraft_server"
+    container_port   = 25565
+  }
+
+  depends_on = [
+    aws_lb_listener.minecraft_listener
+  ]
+
+  tags = {
+    Name    = "${var.project}_service"
+    Project = var.project
+  }
+}
+
+# ---------------------------------------------
 # ECS Task definition
 # ---------------------------------------------
 resource "aws_ecs_task_definition" "minecraft_task" {
